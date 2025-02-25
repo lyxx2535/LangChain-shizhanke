e51f42121e70572040321977c152a0586b105926
lyxx2535 <843683531@qq.com>
2025-02-25 07:18:00 +0000

    update

--- 02_文档QA系统/DocQA.py
+++ 02_文档QA系统/DocQA.py
@@ -118,7 +118,7 @@ def home():
         # 接收用户输入作为问题
         question = request.form.get("question")
 
-        # RetrievalQA链 - 读入问题，生成答案
+        # RetrievalQA链 - 读入问题，生成答案（调上面的qa_chain，很方便）
         result = qa_chain({"query": question})
 
         # 把大模型的回答结果返回网页进行渲染
--- 03_模型IO/01_ModelIO.py
+++ 03_模型IO/01_ModelIO.py
@@ -6,11 +6,11 @@
 import os
 from langchain.prompts import PromptTemplate
 
-# 创建原始模板
+# 创建原始模板 f-string 有占位符
 template = """您是一位专业的鲜花店文案撰写员。\n
 对于售价为 {price} 元的 {flower_name} ，您能提供一个吸引人的简短描述吗？
 """
-# 根据原始模板创建LangChain提示模板
+# 根据原始模板创建LangChain提示模板 直接从一个字符串模板中创建一个PromptTemplate对象
 prompt = PromptTemplate.from_template(template)
 # 打印LangChain提示模板的内容
 print(prompt)
